9fef36cf3e03a57674ad61f3aea4ecb48dc0fd5c
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-06-05 06:59:24 +0200

    add compliment generator solution

---
+++ src/main/kotlin/com/example/compliment/ComplimentGenerator.kt
@@ -0,0 +1,38 @@
+package com.example.compliment
+
+import mu.KotlinLogging
+
+/**
+ * This class is responsible for generating random compliments.
+ * A compliment is a combination of a randomly selected adjective and subject.
+ */
+class ComplimentGenerator {
+    private val logger = KotlinLogging.logger {}
+
+    // Lists of possible adjectives and subjects to use in compliments
+    private val adjectives = listOf("Great", "Amazing", "Incredible", "Fantastic", "Awesome")
+    private val subjects = listOf("job", "work", "effort", "performance", "attitude")
+
+    /**
+     * Generates a random compliment by picking one adjective and one subject randomly.
+     * Logs the generated compliment.
+     *
+     * @return a string representing the generated compliment.
+     */
+    fun generate(): String {
+        val compliment = "${adjectives.random()} ${subjects.random()}"
+        logger.info("Generated compliment: $compliment")
+        return compliment
+    }
+}
+
+/**
+ * The entry point of the application.
+ * Creates a ComplimentGenerator instance and prints a compliment 5 times.
+ */
+fun main() {
+    val generator = ComplimentGenerator()
+    repeat(5) {
+        println("Compliment: ${generator.generate()}")
+    }
+}
